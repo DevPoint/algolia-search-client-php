af8bec29e53dcd2b28b2d8fbf6f48b7f34a60055
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-09 16:23:13 +0200

    added update and delete functions

--- src/SearchClient.php
+++ src/SearchClient.php
@@ -9,6 +9,30 @@ use Devpoint\SearchClient\Contracts\SearchClientBuilder as SearchClientBuilderCo
 class SearchClient implements SearchClientContract {
     
     /**
+     * Update values on the index.
+     * 
+     * @param  string  $index
+     * @param  array   $valuesWithMeta
+     * @return void
+     */
+    public function update($index, $valuesWithMeta)
+    {
+        // TODO: ..
+    }
+
+    /**
+     * Delete values from the index.
+     *
+     * @param  string  $index
+     * @param  array   $ids
+     * @return void
+     */
+    public function delete($index, $ids)
+    {
+        // TODO: ..
+    }
+
+    /**
      * @param  string  $index
      * @param  string  $q
      * @return SearchBuilderContract
